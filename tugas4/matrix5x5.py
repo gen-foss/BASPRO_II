0ec1c270fbf83002932b1c1d26d5845b108d574c
GentaGemintangN <141545108+gen-foss@users.noreply.github.com>
2025-04-16 09:49:36 +0700

    tugas 4

---
+++ tugas4/matrix5x5.py
@@ -0,0 +1,40 @@
+A = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25]
+]
+
+B = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25]
+]
+
+hasil = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+
+for i in range(5):
+    for j in range(5):
+        for k in range(5):
+            hasil[i][j] += A[i][k] * B[k][j]
+
+print("matriks A:")
+for baris in A:
+    print(baris)
+
+print("\nmatriks B:")
+for baris in B:
+    print(baris)
+
+print("\nhasil:")
+for baris in hasil:
+    print(baris)
